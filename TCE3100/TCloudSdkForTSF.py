679e636f461d95c153fee310151a21d57b114a66
ron-dicaprio <love6018@hotmail.com>
2025-02-10 20:08:50 +0800

    Update TCloudSdkForTSF.py

--- TCE3100/TCloudSdkForTSF.py
+++ TCE3100/TCloudSdkForTSF.py
@@ -1,53 +1,20 @@
-#-*- coding:utf-8 -*-
-import yaml,os,json,urllib3
-from tencentcloud.common import credential
-from tencentcloud.common.profile.client_profile import ClientProfile
-from tencentcloud.common.profile.http_profile import HttpProfile
-from tencentcloud.tsf.v20180326 import tsf_client, models
-# disable_warnings()移除警告
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+import json,sys,urllib3
+from tcecloud.common import credential
+from tcecloud.common.profile.client_profile import ClientProfile
+from tcecloud.common.profile.http_profile import HttpProfile
+from tcecloud.common.exception.cloud_sdk_exception import CloudSDKException
+from tcecloud.tsf.v20180326 import tsf_client, models
 urllib3.disable_warnings()
 
 class TSFSdkClient():
-    r""" 
-    : Based On tencentcloud-sdk-python 3.0.1056 .
-    : This Is A Class For TSF .
-    """
-    def ReadYaml(self, YamlPath, MainNode):
-        r"""
-        : 利用PyYAML库读取配置文件
-		: YamlPath     YAML配置文件的绝对路径
-        : MainNode     YAML配置文件中的主节点, 如tsf,tke等
-        : SecretId     腾讯云SecretId
-        : SecretKey    腾讯云SecretKey
-        : endpoint     腾讯云接口调用地址
-        : NamespaceId  应用所属命名空间ID
-        : ClusterId    应用所属集群ID
-        : SubNode=None 可选子节点, 如SecretId,SecretKey,endpoint. 功能性原因暂时禁用该参数
-        """
-	# 下次写的时候记得要控制全局变量，别这么写了
-        global SecretId,SecretKey,tce_region,endpoint,NamespaceId,ClusterId,SubnetId,RepoRoot,scheme
-        try:
-            # 判断是否存在yaml配置文件
-            if os.path.exists(YamlPath):
-                with open(YamlPath, 'r', encoding='utf-8') as file:
-                    config=yaml.safe_load(file)
-                SecretId=config[MainNode]['SecretId']
-                SecretKey=config[MainNode]['SecretKey']
-                tce_region=config[MainNode]['tce_region']
-                endpoint=config[MainNode]['endpoint']
-                NamespaceId=config[MainNode]['NamespaceId']
-                ClusterId=config[MainNode]['ClusterId']
-                SubnetId=config[MainNode]['SubnetId']
-                RepoRoot=config[MainNode]['RepoRoot']
-                scheme=config[MainNode]['scheme']
-                return SecretId,SecretKey,tce_region,endpoint,NamespaceId,ClusterId,SubnetId,RepoRoot,scheme
-            else:
-                print('解析文件路径异常 请确认!!!')
-                exit(1)
-        except Exception as err:
-            print('解析文件异常 请确认!!!', err)
-            exit(1)
-        
+    def __init__(self,tce_region,tce_endpoint,SecretId,SecretKey):
+        self.tce_region=tce_region
+        self.tce_endpoint=tce_endpoint
+        self.SecretId=SecretId
+        self.SecretKey=SecretKey
+
     def tsf_CreateClient(self):
         """
         : 初始化httpProfile
@@ -55,26 +22,26 @@ class TSFSdkClient():
         """
         try:
             # 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
-            cred = credential.Credential(SecretId, SecretKey)
+            cred = credential.Credential(self.SecretId, self.SecretKey)
             # 实例化一个http选项，可选的，没有特殊需求可以跳过
             httpProfile = HttpProfile()
-            httpProfile.protocol = "https"  # 在外网互通的网络环境下支持http协议(默认是https协议),建议使用https协议
+            httpProfile.protocol = "http"  # 在外网互通的网络环境下支持http协议(默认是https协议),建议使用https协议
             httpProfile.keepAlive = True  # 状态保持，默认是False
             httpProfile.reqMethod = "POST"  # get请求(默认为post请求)
             httpProfile.reqTimeout = 60    # 请求超时时间，单位为秒(默认60秒)
             # 跳过证书校验
             httpProfile.certification = False
-            httpProfile.endpoint = endpoint
+            httpProfile.endpoint = self.tce_endpoint
             # 实例化一个client选项，可选的，没有特殊需求可以跳过
             clientProfile = ClientProfile()
             clientProfile.signMethod = "TC3-HMAC-SHA256"  # 指定签名算法
             clientProfile.language = "en-US"  # 指定展示英文（默认为中文）
             clientProfile.httpProfile = httpProfile
-            client = tsf_client.TsfClient(cred, tce_region, clientProfile)
+            client = tsf_client.TsfClient(cred, self.tce_region, clientProfile)
             return client
         except Exception as err:
             return err
-        
+            
     def tsf_CreateApplication(self,ApplicationName):
         """
         : TSF-应用管理-新建应用
@@ -254,7 +221,11 @@ class TSFSdkClient():
             return err
 
 if __name__=="__main__":
-    TSFSdkCientRequest=TSFSdkClient()
-    TSFSdkCientRequest.ReadYaml(YamlPath='./config.yaml',MainNode='tsf')
-    res=TSFSdkCientRequest.tsf_DescribeConfigs('yscsj-cxssb-service')
+    # 生产及预生产都是通过AKSK判定，禁止弄错
+    SecretId='AKIDQbRwVo*********xmGOBdkVx4pFJ1'
+    SecretKey='PTAVU7PLv*********i8CrFNBVbR1IE'
+    tce_region='zj**ywxc'
+    tce_endpoint='tsf.api3.xc11.cloud.*****'
+    TSFSdkCientRequest=TSFSdkClient(tce_region,tce_endpoint,SecretId,SecretKey)
+    res=TSFSdkCientRequest.tsf_CreateApplication('yscsj-demo-service')
     print(res)
